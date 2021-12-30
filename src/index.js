9b963233e001cd3e6e266d3d48d72e5a2e5327f0
Evan Carlson <ecarlson2020@gmail.com>
2021-12-30 01:44:06 -0700

    src/index.js progress getting started

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hey');
+'use strict';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Root extends React.Component{
+	render(){
+		return <div>getting started</div>
+	}
+}
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
