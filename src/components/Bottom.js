3b0446e4f70c2792cd74f79c97cb7000dabc560f
Evan Carlson <ecarlson2020@gmail.com>
2021-12-30 04:03:20 -0700

    right side progress with styling

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -4,7 +4,13 @@ export default class Bottom extends React.Component{
 	render(){
 		return (
 			<div id='bottom' className='bg01 color06 border-radius03'>
-				hello
+				<div className='margin05 padding02'>
+					<div className='relative'>
+						<div className='font02'>Tip Amount</div>
+						<div className='color03 size02 font02'>/ person</div>
+						<div className='color07 size03 font02 big-dollar'>$0.00</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
