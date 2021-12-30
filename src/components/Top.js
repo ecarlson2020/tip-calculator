dd1a0d17ac223395b7c5e1cb66a99d75e8422c20
Evan Carlson <ecarlson2020@gmail.com>
2021-12-30 02:14:58 -0700

    Top component styling progress

--- src/components/Top.js
+++ src/components/Top.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default class Top extends React.Component{
 	render(){
 		return (
-			<div id='top'></div>
+			<div id='top'>
+				<div className='color01'>Bill</div>
+				<div className='color01'>Select Tip</div>
+				<div className='color01'>Number of People</div>
+			</div>
 		)
 	}
 }
