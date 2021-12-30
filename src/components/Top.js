e32272bd3cd823e423e9ff1f92d32883cd7da4ec
Evan Carlson <ecarlson2020@gmail.com>
2021-12-30 15:13:18 -0700

    num people cant be zero functionailty

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export default class Top extends React.Component{
+	state = {
+		num_people: null,
+	}
+
 	draw_tip_amount_boxes(){
 		return [5, 10, 15, 25, 50].map((el, i) => (
 			<div className='tip-box' key={i}>
@@ -9,7 +13,14 @@ export default class Top extends React.Component{
 		))
 	}
 
+	num_people_keyup(){
+		this.setState({
+			num_people: parseInt(document.querySelector("#number-of-people-input").value)
+		});
+	}
+
 	render(){
+		console.log('hey2');
 		return (
 			<div id='top'>
 				<div className='color02 font02'>Bill</div>
@@ -24,10 +35,11 @@ export default class Top extends React.Component{
 						</div>
 					</div>
 				</div>
-				<div className='color02 font02'>Number of People</div>
-				<div className='relative'>
+				<div className={'relative' + (this.state.num_people === 0 ? ' num-people-is-zero' : '')}>
+					<div className='cant-be-zero font02 color08 float-right'>Can&apos;t be zero</div>
+					<div className='color02 font02'>Number of People</div>
 					<img id='person-svg' src='/images/icon-person.svg' />
-					<input className='color01 cursor border-radius02 margin01 margin02 bg05 font02 size01' type='number' placeholder='0' />
+					<input id='number-of-people-input' className='color01 cursor border-radius02 margin01 margin02 bg05 font02 size01' type='number' placeholder='0' onKeyUp={() => this.num_people_keyup()} />
 				</div>
 			</div>
 		)
