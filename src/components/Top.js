90e65fa1b55101f8d1ec1f324461932baf0ec098
Evan Carlson <ecarlson2020@gmail.com>
2021-12-30 03:27:19 -0700

    tip amount styling progress

--- src/components/Top.js
+++ src/components/Top.js
@@ -4,7 +4,7 @@ export default class Top extends React.Component{
 	draw_tip_amount_boxes(){
 		return [5, 10, 15, 25, 50].map((el, i) => (
 			<div className='tip-box'>
-				<div className={'bg01 color06 border-radius02 flex-center font02' + ((i + 1) % 3 !== 0 ? ' margin03' : "")} key={i}>{el}%</div>
+				<div className='bg01 color06 border-radius02 flex-center font02 margin03 padding01' key={i}>{el}%</div>
 			</div>
 		))
 	}
@@ -16,7 +16,7 @@ export default class Top extends React.Component{
 				<img id='bill-dollar-sign' src='/images/icon-dollar.svg' />
 				<input className='border-radius02 margin01 margin02 bg05 font02 size01' type='text' placeholder='0' />
 				<div className='color02 font02'>Select Tip %</div>
-				<div className='margin01 margin02 flex size01'>
+				<div id='tip-amount-container' className='margin01 margin02 flex size01'>
 					{this.draw_tip_amount_boxes()}
 				</div>
 			</div>
