d982b77e59b85b3d2256c03e3ad9cbb8bf7418e4
Evan Carlson <ecarlson2020@gmail.com>
2021-12-30 03:05:37 -0700

    styling select tip progress

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,14 +1,24 @@
 import React from 'react'
 
 export default class Top extends React.Component{
+	draw_tip_amount_boxes(){
+		return [5, 10, 15, 25, 50].map((el, i) => (
+			<div className='margin03 tip-box'>
+				<div className='bg01 color06 border-radius02 flex-center font02' key={i}>{el}%</div>
+			</div>
+		))
+	}
+
 	render(){
 		return (
 			<div id='top'>
 				<div className='color02 font02'>Bill</div>
 				<img id='bill-dollar-sign' src='/images/icon-dollar.svg' />
 				<input className='border-radius02 margin01 margin02 bg05 font02 size01' type='text' placeholder='0' />
-				<div className='color01 font02'>Select Tip %</div>
-				<div className='color01 font02'>Number of People</div>
+				<div className='color02 font02'>Select Tip %</div>
+				<div className='margin01 margin02 flex'>
+					{this.draw_tip_amount_boxes()}
+				</div>
 			</div>
 		)
 	}
