8fa77a52ccabc30cbeaf10e3d2c98afe68017a7e
Evan Carlson <ecarlson2020@gmail.com>
2021-12-30 02:18:23 -0700

    attempting to match greyness color

--- src/components/Top.js
+++ src/components/Top.js
@@ -4,9 +4,9 @@ export default class Top extends React.Component{
 	render(){
 		return (
 			<div id='top'>
-				<div className='color01'>Bill</div>
-				<div className='color01'>Select Tip</div>
-				<div className='color01'>Number of People</div>
+				<div className='color01 font02'>Bill</div>
+				<div className='color01 font02'>Select Tip %</div>
+				<div className='color01 font02'>Number of People</div>
 			</div>
 		)
 	}
