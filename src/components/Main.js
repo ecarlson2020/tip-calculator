2e85ba27fb37d60e48daf0034c692f5d87cd4d44
Evan Carlson <ecarlson2020@gmail.com>
2021-12-30 02:10:41 -0700

    starting Top and Bottom components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Top from './Top'
+import Bottom from './Bottom'
 
 export default class Main extends React.Component{
 	render(){
 		return (
 			<div id='main' className='border-radius01'>
-				hello
+				<Top />
+				<Bottom />
 			</div>
 		)
 	}
