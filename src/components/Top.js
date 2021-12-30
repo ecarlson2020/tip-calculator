825ecf1e52fddfa1ac157d7af00c92c9e6e3c107
Evan Carlson <ecarlson2020@gmail.com>
2021-12-30 02:48:58 -0700

    bill dollar sign

--- src/components/Top.js
+++ src/components/Top.js
@@ -5,7 +5,8 @@ export default class Top extends React.Component{
 		return (
 			<div id='top'>
 				<div className='color02 font02'>Bill</div>
-				<input className='border-radius02 margin01 margin02 bg02 font02 size01' type='text' placeholder='0' />
+				<img id='bill-dollar-sign' src='/images/icon-dollar.svg' />
+				<input className='border-radius02 margin01 margin02 bg05 font02 size01' type='text' placeholder='0' />
 				<div className='color01 font02'>Select Tip %</div>
 				<div className='color01 font02'>Number of People</div>
 			</div>
