7b22d945bf58aa93fe1f85ac13ca94d40e1c286d
Evan Carlson <ecarlson2020@gmail.com>
2021-12-30 02:07:41 -0700

    Main component styling progress

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default class Main extends React.Component{
 	render(){
 		return (
-			<div>hello</div>
+			<div id='main' className='border-radius01'>
+				hello
+			</div>
 		)
 	}
 }
