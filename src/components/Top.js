1473da2813c02006ef42146a3f54aeb46aaee997
Evan Carlson <ecarlson2020@gmail.com>
2021-12-30 02:42:33 -0700

    bg num and color num

--- src/components/Top.js
+++ src/components/Top.js
@@ -4,7 +4,7 @@ export default class Top extends React.Component{
 	render(){
 		return (
 			<div id='top'>
-				<div className='color01 font02'>Bill</div>
+				<div className='color02 font02'>Bill</div>
 				<input className='border-radius02 margin01 margin02 bg02 font02 size01' type='text' placeholder='0' />
 				<div className='color01 font02'>Select Tip %</div>
 				<div className='color01 font02'>Number of People</div>
